a2609adc0b8d6694f2896c1a9374232742fb757d
Sebahattin Demir <info.sebahattindemir@gmail.com>
2022-12-12 19:35:50 +0300

    SQLODEV04

---
+++ Odevler/Odev04.sql
@@ -0,0 +1,24 @@
+--film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
+
+--film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
+
+--film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile
+--başlar ve aynı zamanda rating 'G' ye eşittir?
+select*from film;
+
+--country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
+
+--city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+select * from city;
+
+select distinct replacement_cost from film;
+
+select count(distinct replacement_cost) from film;
+
+select count(title) from film where title like 'T%' and rating='G';
+
+select count(country) from country where country like '_____';
+
+select count(city) from city where city ilike '%r';
+
+
